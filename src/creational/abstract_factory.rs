609540fc483c91ef32cf1360719b8f9dc27ea344
Alexander Smoktal <cosm.ua@gmail.com>
2017-11-14 14:45:59 +0200

    Added currency rate

--- src/creational/abstract_factory.rs
+++ src/creational/abstract_factory.rs
@@ -1,13 +1,15 @@
 use std::option::*;
 
+pub struct USD(f32);
+
 pub trait Coin {
     fn spend(self: Box<Self>);
 }
 
-pub struct BitcoinCoin(u32);
+pub struct BitcoinCoin(f32);
 
 impl BitcoinCoin {
-    fn new(amount: u32) -> Self {
+    fn new(amount: f32) -> Self {
         BitcoinCoin(amount)
     }
 }
@@ -18,10 +20,10 @@ impl Coin for BitcoinCoin {
     }
 }
 
-pub struct EtheriumCoin(u32);
+pub struct EtheriumCoin(f32);
 
 impl EtheriumCoin {
-    fn new(amount: u32) -> Self {
+    fn new(amount: f32) -> Self {
         EtheriumCoin(amount)
     }
 }
@@ -33,65 +35,73 @@ impl Coin for EtheriumCoin {
 }
 
 pub trait Wallet {
-    fn get(&mut self, amount: u32) -> Option<Box<Coin>>;
+    fn get(&mut self, amount: USD) -> Option<Box<Coin>>;
 }
 
-pub struct BitcoinWallet(u32);
+pub struct BitcoinWallet(f32);
 
 impl BitcoinWallet {
-    pub fn new(amount: u32) -> Self {
+    pub fn new(amount: f32) -> Self {
         BitcoinWallet(amount)
     }
 }
 
+static BITCOIN_RATE: f32 = 6468.37;
+
 impl Wallet for BitcoinWallet {
-    fn get(&mut self, amount: u32) -> Option<Box<Coin>> {
-        if self.0 < amount {
+    fn get(&mut self, amount: USD) -> Option<Box<Coin>> {
+        let btc_amount = amount.0 / BITCOIN_RATE;
+
+        if self.0 < btc_amount {
             println!(
                 "Bitcoin wallet doesn't have enough money. Has {}, requested {}",
                 self.0,
-                amount
+                btc_amount
             );
             None
         } else {
-            self.0 -= amount;
-            Some(Box::new(BitcoinCoin::new(amount)))
+            self.0 -= btc_amount;
+            Some(Box::new(BitcoinCoin::new(btc_amount)))
         }
     }
 }
 
-pub struct EtheriumWallet(u32);
+pub struct EtheriumWallet(f32);
 
 impl EtheriumWallet {
-    pub fn new(amount: u32) -> Self {
+    pub fn new(amount: f32) -> Self {
         EtheriumWallet(amount)
     }
 }
 
+static ETHERIUM_RATE: f32 = 318.04;
+
 impl Wallet for EtheriumWallet {
-    fn get(&mut self, amount: u32) -> Option<Box<Coin>> {
-        if self.0 < amount {
+    fn get(&mut self, amount: USD) -> Option<Box<Coin>> {
+        let eth_amount = amount.0 / ETHERIUM_RATE;
+
+        if self.0 < eth_amount {
             println!(
                 "Etherium wallet doesn't have enough money. Has {}, requested {}",
                 self.0,
-                amount
+                eth_amount
             );
             None
         } else {
-            self.0 -= amount;
-            Some(Box::new(EtheriumCoin::new(amount)))
+            self.0 -= eth_amount;
+            Some(Box::new(EtheriumCoin::new(eth_amount)))
         }
     }
 }
 
 pub fn main() {
     let wallet_vec: Vec<Box<Wallet>> = vec![
-        Box::new(BitcoinWallet::new(42)),
-        Box::new(EtheriumWallet::new(42)),
+        Box::new(BitcoinWallet::new(0.02)),
+        Box::new(EtheriumWallet::new(0.4)),
     ];
 
     for mut wallet in wallet_vec {
-        wallet.get(30).map_or((), Coin::spend);
-        wallet.get(15).map_or((), Coin::spend);
+        wallet.get(USD(100.)).map_or((), Coin::spend);
+        wallet.get(USD(100.)).map_or((), Coin::spend);
     }
 }
