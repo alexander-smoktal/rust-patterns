18387f38ca12e5ddd80787a2bfa357f6e45e2061
Alexander Smoktal <cosm.ua@gmail.com>
2017-11-14 19:33:38 +0200

    Added singleton

--- src/creational/mod.rs
+++ src/creational/mod.rs
@@ -1,5 +1,1 @@
-pub mod abstract_factory;
-pub mod builder;
-pub mod factory_method;
-pub mod prototype;
-pub mod singleton;
+
--- src/creational/singleton.rs
+++ src/creational/singleton.rs
@@ -1,1 +1,41 @@
+#[macro_use]
+extern crate lazy_static;
 
+use std::sync::{Mutex, MutexGuard};
+
+lazy_static! {
+    static ref WALLET: Mutex<Wallet> = Mutex::new(Wallet::new());
+}
+
+#[derive(Debug)]
+pub struct Wallet {
+    amount: f64
+}
+
+impl Wallet {
+    fn new() -> Self {
+        println!("Creating a wallet");
+        Wallet {
+            amount: 0f64
+        }
+    }
+
+    fn set_amount(&mut self, amount: f64) {
+        println!("Setting wallet amount: {}", amount);
+        self.amount = amount
+    }
+
+    fn print_amount(&self) {
+        println!("Wallet amount: {}", self.amount);
+    }
+
+    fn get() -> MutexGuard<'static, Wallet> {
+        WALLET.lock().unwrap()
+    }
+}
+
+pub fn main() {
+    Wallet::get().print_amount();
+    Wallet::get().set_amount(42f64);
+    Wallet::get().print_amount();
+}
