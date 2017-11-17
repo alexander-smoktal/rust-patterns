f4b42db5c4950e28f5fcb35a9ae32ac88d4b64cf
Alexander Smoktal <cosm.ua@gmail.com>
2017-11-17 17:59:31 +0200

    Added facade

---
+++ src/structural/facade.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+pub struct USDWallet(f32);
+
+#[derive(Debug)]
+pub struct BitcoinWallet(f32);
+
+#[derive(Debug)]
+pub struct EtheriumWallet(f32);
+
+#[derive(Debug)]
+pub struct Wallets {
+    usd_wallet: USDWallet,
+    btc_wallet: BitcoinWallet,
+    eth_wallet: EtheriumWallet
+}
+
+impl Wallets {
+    pub fn new() -> Self {
+        Wallets {
+            usd_wallet: USDWallet(0f32),
+            btc_wallet: BitcoinWallet(0f32),
+            eth_wallet: EtheriumWallet(0f32)
+        }
+    }
+}
+
+pub fn main() {
+    let wallets = Wallets::new();
+
+    println!("Wallets: {:?}", wallets);
+}
